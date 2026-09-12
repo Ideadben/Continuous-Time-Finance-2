07af67bb3abad6f8912df311ecc613f949ea4566
Ideadben <72947901+Ideadben@users.noreply.github.com>
2026-09-12 23:23:47 +0200

    Uploaded files

---
+++ Hand-in 3/anderson-lake-python-master/tests/test_integration.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import unittest
+import integration
+import numpy as np
+
+class TestExpSinhQuadrature(unittest.TestCase):
+    def test_exp_density(self):
+        scheme = integration.ExpSinhQuadrature(0.5, 1e-8, 100)
+        
+        def exp_dist(x): return np.exp(-x)
+        self.assertAlmostEqual(scheme(exp_dist), 1.)
+        
+class TestGaussianQuadrature(unittest.TestCase):
+    def test_exp_density(self):
+        scheme = integration.GaussianQuadrature(1e-8, 1e-8, 100)
+        
+        def exp_dist(x): return np.exp(-x)
+        self.assertAlmostEqual(scheme(exp_dist), 1.)
+
+if __name__ == '__main__':
+    unittest.main()
